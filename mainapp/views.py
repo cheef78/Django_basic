e599f04ce8cb0ab145521e005048430ebc69f1aa
cheef78 <suslovoleg@mail.ru>
2021-03-02 23:37:39 +0300

    проба тегов и переменных

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def main (request):
-    return render (request, 'mainapp/index.html')
+    content = {
+        'some_name' : 'Oleg',
+        'some_condition' : 0,
+        'variable1' : 'YA YA',
+        'variable2' : 'NO NO',
+        'item_list' : ['basik', 'CS_GO', 'MS_Oficce']
+    }
+    return render (request, 'mainapp/index.html', content)
 
 def products (request):
     return render (request, 'mainapp/products.html')
