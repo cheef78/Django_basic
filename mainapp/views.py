accf6c7f861a527bdf93e686edc05950eaf88326
cheef78 <suslovoleg@mail.ru>
2021-03-06 22:27:00 +0300

    ДЗ2 выполнено в полном объеме

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
+import json
 
 # Create your views here.
 
@@ -25,8 +28,40 @@ def products (request):
     return render (request, 'mainapp/products.html', content)
 
 def contact (request):
+    """ contacts_data = [
+    {
+    'city':'Москва',
+    'phone':'+7-888-888-8888',
+    'email':'info_msk@geekshop.ru',
+    'adress':'Алтуфьевское шоссе, д.16'
+    },
+    {
+        'city':'Иркутск',
+        'phone':'+7-395-248-15425',
+        'email':'info_ikt@geekshop.ru',
+        'adress':'ул. Степана Разина, д.14, оф.34'
+    },
+    {
+        'city':'Сочи',
+        'phone':'+7-124-548-7265',
+        'email':'info_sochi@geekshop.ru',
+        'adress':'Олимпийский проспект, д.24'
+    },
+    {
+        'city':'Караганда',
+        'phone':'+7-666-435-3952',
+        'email':'info_karaganda@geekshop.ru',
+        'adress':'Печальная улица, д.45'
+    } 
+    ]"""
+    contacts_data = []
+    with open (os.path.join(settings.BASE_DIR, 'contacts.json'), 'r', encoding='utf-8') as f:
+        contacts_data = json.load(f)
+    print (f)
+
     content = {
         'some_name' : 'Oleg Suslov',
-        'title' : 'Контакты'
+        'title' : 'Контакты',
+        'contacts_data' : contacts_data
     }
     return render (request, 'mainapp/contact.html', content)
