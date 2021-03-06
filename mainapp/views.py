a97bcec78777f27f3c1c50bf191e901d177dbd2c
cheef78 <suslovoleg@mail.ru>
2021-03-06 20:43:00 +0300

    выполнены все задания из ДЗ2

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,29 +5,28 @@ from django.shortcuts import render
 def main (request):
     content = {
         'some_name' : 'Oleg Suslov',
-        'some_condition' : 0,
-        'variable1' : 'YA YA',
-        'variable2' : 'NO NO',
-        'item_list' : ['basik', 'CS_GO', 'MS_Oficce']
+        'title' : 'Главная'
     }
     return render (request, 'mainapp/index.html', content)
 
 def products (request):
+    links_menu = [
+        {'href':'products_all', 'name':'Все'},
+        {'href':'products_home', 'name':'Для дома'},
+        {'href':'products_office', 'name':'Для офиса'},
+        {'href':'products_modern', 'name':'Современный стиль'},
+        {'href':'products_classic', 'name':'Классический стиль'}
+    ]
     content = {
         'some_name' : 'Oleg Suslov',
-        'some_condition' : 0,
-        'variable1' : 'YA YA',
-        'variable2' : 'NO NO',
-        'item_list' : ['basik', 'CS_GO', 'MS_Oficce']
+        'title' : 'Каталог',
+        'links_menu': links_menu
     }
     return render (request, 'mainapp/products.html', content)
 
 def contact (request):
     content = {
         'some_name' : 'Oleg Suslov',
-        'some_condition' : 0,
-        'variable1' : 'YA YA',
-        'variable2' : 'NO NO',
-        'item_list' : ['basik', 'CS_GO', 'MS_Oficce']
+        'title' : 'Контакты'
     }
     return render (request, 'mainapp/contact.html', content)
