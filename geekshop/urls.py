5722a9c5db926be7365d93ffc51da2cd2bbec6ac
cheef78 <suslovoleg@mail.ru>
2021-03-07 18:15:21 +0300

    начата работа по ДЗ_3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -33,3 +35,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls, name = 'admin'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+     
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(unique=True, verbose_name='имя', max_length=64)
+    descript = models.TextField(verbose_name='описание', blank=True )
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя', max_length=128)
+    image = models.ImageField(blank = True, upload_to='products_images')
+    short_desc = models.CharField(verbose_name='краткое описание', max_length=64, blank=True)
+    descript = models.TextField(blank=True, verbose_name='полное описание')
+    price = models.DecimalField(verbose_name='цена, руб.', decimal_places=0, max_digits=8, default=0)
+    quantity = models.SmallIntegerField(verbose_name='количество на складе, шт.', default=0)
+
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+            
