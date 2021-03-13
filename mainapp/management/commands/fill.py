de9ace14a9695393db8b3562a929b1ee0a305e97
cheef78 <suslovoleg@mail.ru>
2021-03-13 17:04:38 +0300

    делаю загрузку в базу из ДжСОн

---
+++ mainapp/management/commands/fill.py
@@ -0,0 +1,22 @@
+import os
+import json
+from django.core.management import BaseCommand
+from django.conf import settings
+from mainapp.models import Product
+
+def load_from_json(file_name):
+    with open(os.path.join (settings.BASE_DIR, f'mainapp/json/{file_name}.json'), 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+
+
+class Command (BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+        # print (categories)
+        ProductCategory.objects.all().delete()
+        for cat in categories:
+            ProductCategory.objects.create(**cat)
+
+
