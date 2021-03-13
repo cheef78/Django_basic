f943e903df2c1bf9392757e2b7d16b3c29bcfb6f
cheef78 <suslovoleg@mail.ru>
2021-03-13 19:45:58 +0300

    делаем админку

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -1,8 +1,9 @@
-import os
 import json
 from django.core.management import BaseCommand
 from django.conf import settings
-from mainapp.models import ProductCategory
+import os
+from mainapp.models import ProductCategory, Product
+from django.contrib.auth.models import User
 
 def load_from_json(file_name):
     with open(os.path.join (settings.BASE_DIR, f'mainapp/json/{file_name}.json'), 'r', encoding='utf-8') as f:
@@ -18,5 +19,14 @@ class Command (BaseCommand):
         ProductCategory.objects.all().delete()
         for cat in categories:
             ProductCategory.objects.create(**cat)
+        products = load_from_json('products')
+        Product.objects.all().delete()
+        for prod in products:
+            _cat = ProductCategory.objects.get(name=prod['category'])
+            prod['category'] = _cat
+            Product.objects.create(**prod)
+        User.objects.create_superuser ('django','django@local.db', 'geekbrains')
+
+       
 
 
