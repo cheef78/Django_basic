80164f7da76bf85210077222586f2509b7c7f5a1
cheef78 <suslovoleg@mail.ru>
2021-03-17 17:46:55 +0300

    сформирована выдача продуктов по выбору категории

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from mainapp.models import Product, ProductCategory
-
+from django.shortcuts import get_object_or_404
 import os
 import json
 
@@ -17,22 +17,22 @@ def main (request):
     }
     return render (request, 'mainapp/index.html', content)
 
-def products (request, pk = None):
+""" def products (request, pk = None):
    
-    """ links_menu = [
+    links_menu = [
         {'href':'products_all', 'name':'Все'},
         {'href':'products_home', 'name':'Для дома'},
         {'href':'products_office', 'name':'Для офиса'},
         {'href':'products_modern', 'name':'Современный стиль'},
         {'href':'products_classic', 'name':'Классический стиль'}
-    ] """
+    ] 
     links_menu = ProductCategory.objects.all()
     content = {
         'some_name' : 'Oleg Suslov',
         'title' : 'Каталог',
         'links_menu': links_menu
     }
-    return render (request, 'mainapp/products.html', content)
+    return render (request, 'mainapp/products.html', content) """
 
 def contact (request):
     
@@ -47,3 +47,31 @@ def contact (request):
         'contacts_data' : contacts_data
     }
     return render (request, 'mainapp/contact.html', content)
+
+
+
+def products (request, pk=None):
+    print(pk)
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by( 'price' )
+            category = { 'name' : 'все' }
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by( 'price' )
+        content = {
+            'title' : title,
+            'links_menu' : links_menu,
+            'category' : category,
+            'products' : products,
+        }
+        return render(request, 'mainapp/products_list.html' , content)
+    same_products = Product.objects.all()[ 3 : 5 ]
+    content = {
+        'title' : title,
+        'links_menu' : links_menu,
+        'same_products' : same_products
+    }
+    return render(request, 'mainapp/products.html' , content)
